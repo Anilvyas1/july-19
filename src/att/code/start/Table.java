622353fa0e08120c86e87d1d7e71140eb7dd42f9
“Anilvyas1” <anilsharma0505@gmail.com>
2020-07-19 16:14:18 +0530

    second change

---
+++ src/att/code/start/Table.java
@@ -0,0 +1,13 @@
+package att.code.start;
+
+public class Table {
+	static int a=3;
+
+	public static void main(String[] args) {
+		for(int i=1;i<11;i++) {
+		System.out.println("Table::  "+ i*a);
+		}
+
+	}
+
+}
