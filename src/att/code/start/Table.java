cef48f8d46fd6c1bd4b9174c9a1ca9a0ff63f956
“Anilvyas1” <anilsharma0505@gmail.com>
2020-07-19 16:19:39 +0530

    second change again

--- src/att/code/start/Table.java
+++ src/att/code/start/Table.java
@@ -1,9 +1,13 @@
 package att.code.start;
 
+import java.util.Date;
+
 public class Table {
 	static int a=3;
 
 	public static void main(String[] args) {
+		Date d=new Date();
+		System.out.println("Date:::  "+d);
 		for(int i=1;i<11;i++) {
 		System.out.println("Table::  "+ i*a);
 		}
