8530aa03b47f667b18019ac94bc83cb6a76d1eea
“Anilvyas1” <anilsharma0505@gmail.com>
2020-07-19 16:25:55 +0530

    commit dev branch

---
+++ src/att/code/start/Date11.java
@@ -0,0 +1,13 @@
+package att.code.start;
+
+import java.util.Date;
+
+public class Date11 {
+
+	public static void main(String[] args) {
+		Date d=new Date();
+		System.out.println("Today date::::  "+d);
+
+	}
+
+}
